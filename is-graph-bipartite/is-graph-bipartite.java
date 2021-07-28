f51732d491632cd1a3dcf9afc244efcd36f4d21d
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-07-28 20:27:48 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (46.79%) - LeetHub

--- is-graph-bipartite/is-graph-bipartite.java
+++ is-graph-bipartite/is-graph-bipartite.java
@@ -1,23 +1,14 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
          ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int A = graph.length;
-        for(int i=0;i<A;i++){
-            adj.add(new ArrayList<Integer>());
-        }
-        
-        for(int i=0;i<graph.length;i++){
-           for(int j=0;j<graph[i].length;j++){
-               adj.get(i).add(graph[i][j]);
-               adj.get(graph[i][j]).add(i);
-           }
-        }
+        int A = graph.length;        
+       
         int [] vis = new int[A];
         Arrays.fill(vis,-1);
         
-        for(int i=0;i<adj.size();i++){
+        for(int i=0;i<A;i++){
             if(vis[i]==-1){
-                dfs(adj,i,i,1,vis);
+                dfs(graph,i,i,1,vis);
                 if(!flag)
                     return false;
             }
@@ -27,13 +18,13 @@ class Solution {
     }
     
     boolean flag = true;
-    public void dfs( ArrayList<ArrayList<Integer>> adj, int cur, int parent, int c, int[] visited){
+    public void dfs( int[][] graph, int cur, int parent, int c, int[] visited){
       
         visited[cur]=c;
-        for(int i=0;i<adj.get(cur).size();i++){
-            int v = adj.get(cur).get(i);
+        for(int i=0;i<graph[cur].length;i++){
+            int v = graph[cur][i];
             if(visited[v]==-1){
-                dfs(adj, v, parent,3-c,visited);
+                dfs(graph, v, cur,3-c,visited);
             }
             else if(v!=parent && visited[v]==c){
                 flag = false;
