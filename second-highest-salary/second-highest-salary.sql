dd80ea923521f64f5aeb70d57397a2a6dc2d2a30
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-08-04 15:42:47 +0530

    Time: 195 ms (44.46%), Space: 0B (100.00%) - LeetHub

--- second-highest-salary/second-highest-salary.sql
+++ second-highest-salary/second-highest-salary.sql
@@ -1,3 +1,2 @@
-/* Write your PL/SQL query statement below */
+# Write your MySQL query statement below
 select max(Salary) as SecondHighestSalary  from Employee where Salary <> (select max(Salary) from Employee)
-
