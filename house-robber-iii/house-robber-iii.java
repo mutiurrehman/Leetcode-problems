e1fe6805cf552d23664cddb2e8414ea459c380b4
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-08-13 16:01:26 +0530

    Time: 3 ms (16.77%), Space: 41.7 MB (5.42%) - LeetHub

---
+++ house-robber-iii/house-robber-iii.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<TreeNode, Integer> map = new HashMap<>();
+    public int rob(TreeNode root) {
+    
+        if(root==null)
+            return 0;
+        if(map.containsKey(root))
+            return map.get(root);
+        int val=0;
+        if(root.left!=null){
+            val+=rob(root.left.left) + rob(root.left.right);
+        }
+        
+        if(root.right!=null){
+            val+=rob(root.right.left)+ rob(root.right.right);
+        }
+        
+        map.put(root,Math.max(val+root.val,rob(root.left)+rob(root.right)));
+        return map.get(root);
+    }
+}
