dccaa889ac465f568dcb680adf7c25ec29bc2b82
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-08-17 10:36:55 +0530

    Time: 6 ms (44.83%), Space: 37.3 MB (98.02%) - LeetHub

--- regular-expression-matching/regular-expression-matching.java
+++ regular-expression-matching/regular-expression-matching.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isMatch(String s, String p) {
         
         boolean[][] dp = new boolean[p.length()+1][s.length()+1];
-         
+        
         for(int i=0;i<dp.length;i++){
             for(int j=0;j<dp[i].length;j++){
                 if(i==0 && j==0){
@@ -13,31 +13,30 @@ class Solution {
                 }
                 else if(j==0){
                     if(p.charAt(i-1)=='*')
-                    dp[i][j]=dp[i-2][j];
+                        dp[i][j] = dp[i-2][j];
                 }
-                else{
+                else {
                     char pc = p.charAt(i-1);
                     char sc = s.charAt(j-1);
                     
-                    if(pc=='*' ){
+                    if(pc=='*'){
                         dp[i][j]=dp[i-2][j];
-                        if(p.charAt(i-2)==sc || p.charAt(i-2)=='.' ){
-                            dp[i][j]=dp[i][j]||dp[i][j-1];
-                        }
-                    }
-                    else if(pc=='.'){
-                        dp[i][j]=dp[i-1][j-1];
+                        if(p.charAt(i-2)==sc|| p.charAt(i-2)=='.')
+                        dp[i][j]|=dp[i][j-1];
                     }
                     else if(pc==sc){
+                        dp[i][j]=dp[i-1][j-1];
+                    }
+                    else if(pc=='.'){
                         dp[i][j]=dp[i-1][j-1];
                     }
                     else{
                         dp[i][j]=false;
                     }
                 }
+                
             }
         }
-        
         return dp[p.length()][s.length()];
     }
 }
