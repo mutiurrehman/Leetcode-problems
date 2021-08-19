b9f1c204121433cc9fac318a68da7a5ec794ea4b
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-08-19 23:02:15 +0530

    Time: 14 ms (72.16%), Space: 38.9 MB (81.12%) - LeetHub

--- all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -8,82 +8,56 @@
  * }
  */
 class Solution {
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        graph(root);
-        HashSet<TreeNode> visited = new HashSet<>();
-        dfs(map,0,k,visited,target);
-        return ans;
-    }
-    
-    List<Integer> ans = new ArrayList<>();
-    
-    public void dfs(HashMap<TreeNode, ArrayList<TreeNode>> map, int curDist, int k , HashSet<TreeNode> visited, TreeNode u){
-        if(k==curDist){
-            ans.add(u.val);
-           // return;
-        }
-        
-        visited.add(u);
-        if(map.get(u)!=null){
-        for(int i=0;i<map.get(u).size();i++){
-            TreeNode v = map.get(u).get(i);
-            if(!visited.contains(v)){
-                dfs(map,curDist+1,k,visited,v);
-            }
-        }
-        }
-        //curDist=0;
-    }
-    
+    Map<TreeNode, TreeNode> parent;
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        parent = new HashMap();
+        dfs(root, null);
 
-    HashMap<TreeNode, ArrayList<TreeNode>> map = new HashMap<>();
-    public void graph(TreeNode root){
-        if(root==null)
-            return;
-        
-        if(root.left!=null){
-            if(map.containsKey(root)){
-                map.get(root).add(root.left);
-                if(map.containsKey(root.left))
-                    map.get(root.left).add(root);
-                else{
-                    map.put(root.left,new ArrayList<TreeNode>());
-                    map.get(root.left).add(root);
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(null);
+        queue.add(target);
+
+        Set<TreeNode> seen = new HashSet();
+        seen.add(target);
+        seen.add(null);
+
+        int dist = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                if (dist == K) {
+                    List<Integer> ans = new ArrayList();
+                    for (TreeNode n: queue)
+                        ans.add(n.val);
+                    return ans;
                 }
-            } else{
-                map.put(root,new ArrayList<TreeNode>());
-                map.get(root).add(root.left);
-                if(map.containsKey(root.left))
-                    map.get(root.left).add(root);
-                else{
-                    map.put(root.left,new ArrayList<TreeNode>());
-                    map.get(root.left).add(root);
+                queue.offer(null);
+                dist++;
+            } else {
+                if (!seen.contains(node.left)) {
+                    seen.add(node.left);
+                    queue.offer(node.left);
                 }
-            }
-        }
-        
-         if(root.right!=null){
-            if(map.containsKey(root)){
-                map.get(root).add(root.right);
-                if(map.containsKey(root.right))
-                    map.get(root.right).add(root);
-                else{
-                    map.put(root.right,new ArrayList<TreeNode>());
-                    map.get(root.right).add(root);
+                if (!seen.contains(node.right)) {
+                    seen.add(node.right);
+                    queue.offer(node.right);
                 }
-            } else{
-                map.put(root,new ArrayList<TreeNode>());
-                map.get(root).add(root.right);
-                if(map.containsKey(root.right))
-                    map.get(root.right).add(root);
-                else{
-                    map.put(root.right,new ArrayList<TreeNode>());
-                    map.get(root.right).add(root);
+                TreeNode par = parent.get(node);
+                if (!seen.contains(par)) {
+                    seen.add(par);
+                    queue.offer(par);
                 }
             }
         }
-        
-        graph(root.left);
-        graph(root.right);
+
+        return new ArrayList<Integer>();
+    }
+
+    public void dfs(TreeNode node, TreeNode par) {
+        if (node != null) {
+            parent.put(node, par);
+            dfs(node.left, node);
+            dfs(node.right, node);
+        }
     }
 }
