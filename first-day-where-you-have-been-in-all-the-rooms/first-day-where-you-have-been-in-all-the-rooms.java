c5119c42dfcbf8b0f572395056821202735cfbca
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-09-05 13:18:13 +0530

    Time: 12 ms (100.00%), Space: 54.7 MB (50.00%) - LeetHub

---
+++ first-day-where-you-have-been-in-all-the-rooms/first-day-where-you-have-been-in-all-the-rooms.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int firstDayBeenInAllRooms(int[] nv) {
+        int len = nv.length;
+        long[] dp = new long[len];
+        dp[1] = 2;
+        int mod = 1000000007;
+        for(int i = 2 ; i < len ; i++){
+            dp[i] = dp[i-1] + dp[i-1] + mod - dp[nv[i-1]] + 2;
+            dp[i] %= mod;
+        }
+        return (int)dp[len-1];
+    }
+}
