cca8b583ee6c0815425bc57cd85a0ba003fa5b91
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-09-12 14:53:15 +0530

    Time: 98 ms (13.18%), Space: 79.9 MB (11.68%) - LeetHub

--- minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
+++ minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,38 +1,18 @@
 class Solution {
-    public int minSwaps(String expr) {
-        
-    int len = expr.length();
-        int ans;
-       
-        // Expressions of odd lengths
-        // cannot be balanced
-        if (len % 2 != 0) {
-            return -1;
-        }
-        int left_brace = 0, right_brace = 0;
-        for (int i = 0; i < len; i++) {
-            char ch = expr.charAt(i);
-           
-            // If we find a left bracket then we simply
-            // increment the left bracket
-            if (ch == '[') {
-                left_brace++;
-            }
-           
-            // Else if left bracket is 0 then we find
-            // unbalanced right bracket and increment
-            // right bracket or if the expression
-            // is balanced then we decrement left
+    public int minSwaps (String s) {
+        Stack <Character> stack = new Stack ();
+        int mismatch = 0;
+        for (int i = 0; i < s.length (); i++) {
+            char ch = s.charAt (i);
+            if (ch == '[')
+                stack.push (ch);
             else {
-                if (left_brace == 0) {
-                    right_brace++;
-                }
-                else {
-                    left_brace--;
-                }
+                if (!stack.isEmpty ())
+                    stack.pop ();
+                else 
+                    mismatch++;
             }
         }
-        
-        return (left_brace+1)/2;
+        return (mismatch + 1) / 2;
     }
 }
