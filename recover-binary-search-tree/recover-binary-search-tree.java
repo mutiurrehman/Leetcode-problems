ec1f07bbd915d2e433f88c42687cddb4035bb96d
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-09-15 15:31:45 +0530

    Time: 2 ms (86.91%), Space: 39.2 MB (74.20%) - LeetHub

--- recover-binary-search-tree/recover-binary-search-tree.java
+++ recover-binary-search-tree/recover-binary-search-tree.java
@@ -1,36 +1,44 @@
-public class Solution {
-    
-    TreeNode first = null;
-    TreeNode second = null;
-    
-    TreeNode prev =null;
-    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private TreeNode first;
+    private TreeNode second;
+    private TreeNode pre;
     public void recoverTree(TreeNode root) {
-           if(root==null) return;
-        traverse(root);
-
+        if(root==null) return;
+        first = null;
+        second = null;
+        pre = null;
+        inorder(root);
         int temp = first.val;
         first.val = second.val;
         second.val = temp;
     }
     
-    private void traverse(TreeNode root) {
+    private void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
         
-        if (root == null)
-            return;
-            
-        traverse(root.left);
-        
-        if (first == null &&  (prev==null ||prev.val >= root.val)) {
-            first = prev;
+        if(first==null && (pre==null ||pre.val>=root.val)){
+            first = pre;
         }
-    
-        if (first != null && prev.val >= root.val) {
+        if(first!=null && pre.val>=root.val){
             second = root;
-        }        
-        prev = root;
-
-
-        traverse(root.right);
-}
+        }
+        pre = root;
+        inorder(root.right);
+    }
 }
