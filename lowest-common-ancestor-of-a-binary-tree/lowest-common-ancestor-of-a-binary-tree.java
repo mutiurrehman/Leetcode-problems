b4b0cf482a2d9d6fe640cefe134556475ec8d43b
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-09-16 18:10:12 +0530

    Time: 13 ms (16.27%), Space: 45.6 MB (6.08%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -1,25 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
-    private TreeNode ans= null;
-    private boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
-
-        if (currentNode == null) {
-            return false;
-        }
-
-        int left = recurseTree(currentNode.left, p, q) ? 1 : 0;
-        int right = recurseTree(currentNode.right, p, q) ? 1 : 0;
-        int mid = (currentNode == p || currentNode == q) ? 1 : 0;
-        
-        if (mid + left + right >= 2) {
-            this.ans = currentNode;
-        }
-        
-        return (mid + left + right > 0);
-    }
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-        recurseTree(root, p, q);
+        helper(root,p,q);
         return ans;
+    }
+    
+    TreeNode ans = null;
+    public boolean helper(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null)
+            return false;
+        
+        
+        
+        int f = helper(root.left,p,q) ? 1 : 0;
+        int s = helper(root.right,p,q)? 1 : 0;
+        int t = (root == p|| root==q) ? 1 : 0;
+        
+        if((f+s+t)>=2){
+            this.ans = root;   
+        }
+        
+        return (f+s+t)>0;
     }
 }
