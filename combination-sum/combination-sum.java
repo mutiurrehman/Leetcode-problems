6837adac774065e0d26e03df2143024f49e42d38
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-09-27 13:10:48 +0530

    Time: 5 ms (46.63%), Space: 39.4 MB (53.86%) - LeetHub

--- combination-sum/combination-sum.java
+++ combination-sum/combination-sum.java
@@ -1,24 +1,21 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
-        Arrays.sort(candidates);
-        List<List<Integer>> res = new ArrayList<>();
-        helper(res, new ArrayList<Integer>(),candidates,0,target);
-        return res;
-    }
-    
-    public void helper(List<List<Integer>> res, ArrayList<Integer> temp, int[] nums, int start, int remain){
-        if(remain<0)
-            return;
-        else if(remain==0 && !res.contains(new ArrayList<Integer>(temp)))
-        res.add(new ArrayList<Integer>(temp));
-        else{
-            for(int i=start;i<nums.length;i++){
-                temp.add(nums[i]);
-                helper(res,temp,nums,i,remain-nums[i]);
-                temp.remove(temp.size()-1);
-            }
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, target, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+    if(remain < 0) return;
+    else if(remain == 0) list.add(new ArrayList<>(tempList));
+    else{ 
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+            tempList.remove(tempList.size() - 1);
         }
     }
+}
     
 }
