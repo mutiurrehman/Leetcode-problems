3a00f0373dbde203de9402b539b19976c7e3c150
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-10-02 21:39:48 +0530

    Time: 3 ms (26.84%), Space: 41.1 MB (18.29%) - LeetHub

--- dungeon-game/dungeon-game.java
+++ dungeon-game/dungeon-game.java
@@ -1,23 +1,24 @@
 class Solution {
     public int calculateMinimumHP(int[][] A) {
-    int[][] dp = new int[A.length][A[0].length];
+        int[][] dp = new int[A.length][A[0].length];
         
         for(int i=dp.length-1;i>=0;i--){
             for(int j=dp[0].length-1;j>=0;j--){
                 if(i==dp.length-1 && j==dp[0].length-1){
-                    dp[i][j]=Math.min(0,A[i][j]);
+                    dp[i][j] = Math.min(0, A[i][j]);
                 }
                 else if(i==dp.length-1){
-                    dp[i][j]=Math.min(0, A[i][j]+dp[i][j+1]);
+                    dp[i][j] = Math.min(0, dp[i][j+1]+A[i][j]);
                 }
                 else if(j==dp[0].length-1){
-                    dp[i][j]=Math.min(0, A[i][j]+dp[i+1][j]);
+                    dp[i][j] = Math.min(0, dp[i+1][j]+A[i][j]);
                 }
                 else{
-                    dp[i][j]=Math.min(0, A[i][j]+Math.max(dp[i+1][j],dp[i][j+1]));
+                    dp[i][j] = Math.min(0,A[i][j]+Math.max(dp[i+1][j],dp[i][j+1]));
                 }
             }
         }
+        
         return Math.abs(dp[0][0])+1;
     }
 }
