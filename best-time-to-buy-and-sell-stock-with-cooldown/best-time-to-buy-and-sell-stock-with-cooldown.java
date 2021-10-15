1f28f67c202323af11041e8f4fb90987fc56573d
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-10-15 13:58:50 +0530

    Time: 1 ms (65.12%), Space: 39 MB (31.13%) - LeetHub

--- best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,34 +1,34 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int obsp=-prices[0],ossp=0,ocsp=0;
+        int obsp = - prices[0], ossp = 0, ocsp=0;
         
-        for(int i =0;i<prices.length;i++){
-            int nbsp=0,nssp=0,ncsp=0;
+        for(int i=0;i<prices.length;i++){
+            int nbsp=0,nssp=0, ncsp=0;
             
-            if(ocsp-prices[i]>obsp){
-                nbsp=ocsp-prices[i];
+            if(ocsp - prices[i]>obsp){
+                nbsp = ocsp - prices[i];
             }
             else{
-                nbsp=obsp;
+                nbsp = obsp;
             }
             
             if(obsp+prices[i]>ossp){
-                nssp=obsp+prices[i];
+                nssp = obsp + prices[i];
             }
             else{
-                nssp=ossp;
+                nssp = ossp;
             }
             
             if(ossp>ocsp){
-                ncsp=ossp;
+                ncsp = ossp;
             }
             else{
-                ncsp=ocsp;
+                ncsp = ocsp;
             }
             
-            obsp=nbsp;
-            ossp=nssp;
-            ocsp=ncsp;
+            obsp = nbsp;
+            ocsp = ncsp;
+            ossp = nssp;
         }
         
         return ossp;
