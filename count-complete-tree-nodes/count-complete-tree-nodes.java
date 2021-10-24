06da8e7c2659cf1b5dec7eeb66d86bb92b798d5d
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-10-24 20:31:21 +0530

    Time: 0 ms (100.00%), Space: 47.3 MB (10.98%) - LeetHub

--- count-complete-tree-nodes/count-complete-tree-nodes.java
+++ count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,12 +14,13 @@
  * }
  */
 class Solution {
+    int height(TreeNode root) {
+        return root == null ? -1 : 1 + height(root.left);
+    }
     public int countNodes(TreeNode root) {
         int h = height(root);
-        
-        return h<0 ? 0 : height(root.right) == h-1 ? (1<<h) +countNodes(root.right) : (1<<h-1) + countNodes(root.left);
-    }
-    public int height(TreeNode root){
-        return root==null ? -1 : 1 + height(root.left);
+        return h < 0 ? 0 :
+               height(root.right) == h-1 ? (1 << h) + countNodes(root.right)
+                                         : (1 << h-1) + countNodes(root.left);
     }
 }
