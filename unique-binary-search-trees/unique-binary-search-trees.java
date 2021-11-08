8ac9bb1a449e6dd2d8c934073c1b5464711a7a22
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-11-08 09:28:32 +0530

    Time: 0 ms (100.00%), Space: 35.8 MB (65.77%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.java
+++ unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,12 +1,12 @@
 class Solution { 
     public int numTrees(int n) {
      
-      int[] dp = new int[n+1];
-        
-        dp[0]=1;dp[1]=1;
-        for(int i=2;i<=n;i++){
-            int l=0;
-            int r =i-1;
+        int[] dp =new int[n+1];
+        dp[0]= 1;
+        dp[1] = 1;
+        for(int i=2;i<n+1;i++){
+            int l = 0, r = i-1;
+            
             while(l<=i-1){
                 dp[i]+=dp[l]*dp[r];
                 l++;
