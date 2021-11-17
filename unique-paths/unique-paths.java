522c4c61dce28b1d97a96cc08a8694e7fe2dd352
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-11-17 11:52:56 +0530

    Time: 0 ms (100.00%), Space: 38.4 MB (6.31%) - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -1,12 +1,12 @@
 class Solution {
-    
-    Integer[][] dp = new Integer[101][101];
-    public int uniquePaths(int i, int j) {
-     if(i==1 || j==1)
-         return 1;
-        if(dp[i][j]!=null)
-            return dp[i][j];
+    int[][] dp = new int[101][101];
+    public int uniquePaths(int m, int n) {
+        if(m==1 || n==1)
+            return 1;
         
-        return dp[i][j]=uniquePaths(i,j-1)+uniquePaths(i-1,j);
+        if(dp[m][n]!=0)
+            return dp[m][n];
+        return dp[m][n] = uniquePaths(m-1,n)+uniquePaths(m,n-1);
     }
+    
 }
