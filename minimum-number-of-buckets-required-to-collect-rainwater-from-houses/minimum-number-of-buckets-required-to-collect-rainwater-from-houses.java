3746894181be86c3913be9a2153dbff3db46fc15
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-11-28 13:39:58 +0530

    Time: 4 ms (100.00%), Space: 40 MB (60.00%) - LeetHub

---
+++ minimum-number-of-buckets-required-to-collect-rainwater-from-houses/minimum-number-of-buckets-required-to-collect-rainwater-from-houses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumBuckets(String street) {
+        int count=0;
+        for(int i=0;i<street.length();i++){
+            if(street.charAt(i)=='H'){
+                if(i+1<street.length() && street.charAt(i+1)=='.'){
+                    count++;
+                    i+=2;
+                    continue;
+                }
+                if(i-1>=0 && street.charAt(i-1)=='.'){
+                    count++;
+                    continue;
+                }
+                return -1;
+            }
+        }
+        return count;
+    }
+}
