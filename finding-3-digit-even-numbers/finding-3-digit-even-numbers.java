dd2f2e023000b28d8057ff79420a23ccf4866532
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2021-12-10 21:40:02 +0530

    Time: 5 ms (90.65%), Space: 39 MB (97.12%) - LeetHub

--- finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
+++ finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -1,55 +1,29 @@
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        HashSet<Integer> ans = new HashSet<>();
-        
-        int n = digits.length;
-        for(int i=0;i<n;i++){
-            if(digits[i]%2!=0)
-                continue;
-            
-            int cur = digits[i];
-            int[] temp = new int[3];
-            temp[2] = cur;
-            
-            for(int j=0;j<n;j++){
-                if(j==i)
-                    continue;
-                for(int k=0;k<n;k++){
-                    if(k==i || k==j)
-                        continue;
-                    
-                    if(digits[j]!=0 && digits[k]!=0){
-                        temp[0] = digits[j];
-                        temp[1] = digits[k];
-                        int num=temp[0]*100+temp[1]*10+temp[2];
-                        ans.add(num);
-                        temp[0] = digits[k];
-                        temp[1] = digits[j];
-                        num=temp[0]*100+temp[1]*10+temp[2];
-                        ans.add(num);
-                    }
-                    else if(digits[j]!=0){
-                        temp[0] = digits[j];
-                        temp[1] = digits[k];
-                        int num=temp[0]*100+temp[1]*10+temp[2];
-                        ans.add(num);
-                    }
-                    else if(digits[k]!=0){
-                        temp[0] = digits[k];
-                        temp[1] = digits[j];
-                        int num=temp[0]*100+temp[1]*10+temp[2];
-                        ans.add(num);
-                    }
-                }
+        int[] counter = new int[10];
+        for (int d : digits) 
+            counter[d] ++;
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 100; i < 1000; i += 2){
+            int d0 = i % 10, d1 = (i / 10) % 10, d2 = i / 100;
+
+            counter[d0] --;
+            counter[d1] --;
+            counter[d2] --;
+            if (counter[d0] >= 0 && counter[d1] >= 0 && counter[d2] >= 0){
+                list.add(i);
             }
+
+            counter[d0] ++;
+            counter[d1] ++;
+            counter[d2] ++;
         }
+
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i ++) 
+            res[i] = list.get(i);
         
-        int[] res = new int[ans.size()];
-        int j=0;
-        for(int i:ans){
-            res[j++] =  i;
-        }
-        Arrays.sort(res);
         return res;
     }
 }
