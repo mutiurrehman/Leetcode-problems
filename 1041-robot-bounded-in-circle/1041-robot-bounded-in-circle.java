7d6c17ae2ee094cd6907b57b6393d65d5c4e2cbc
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2022-01-09 12:34:22 +0530

    Time: 2 ms (10.05%), Space: 38.4 MB (27.32%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
@@ -1,25 +1,52 @@
 class Solution {
-    public boolean isRobotBounded(String str) {
+    public boolean isRobotBounded(String instructions) {
         
-        int x=0, y=0, dir=0;
-        int[][] dirs ={{0,1},{1,0},{0,-1},{-1,0}};
-        
-        for(int i=0;i<str.length();i++){
-            char cc = str.charAt(i);
-            if(cc=='R'){
-                dir=(dir+1)%4;
-                
+        if (instructions.length() == 0)
+            return false;
+        int x = 0;
+        int y = 0;  // initial points of the robot
+        String directions = "North"; // initial direction of robot
+        /*
+                    North
+            West                East
+                    South
+                    
+        */
+        for (char ch: instructions.toCharArray()) {
+            if (ch == 'G') {
+                if (directions.equals("North"))
+                    y += 1;
+                else if (directions.equals("South"))
+                    y -= 1;
+                else if(directions.equals("East"))
+                    x += 1;
+                else
+                    x -= 1;
             }
-            else if(cc=='L'){
-                dir = (dir+3)%4;
+            else if (ch == 'L') {
+                if (directions.equals("North"))
+                    directions = "West";
+                else if (directions.equals("West"))
+                    directions = "South";
+                else if (directions.equals("South"))
+                    directions = "East";
+                else directions = "North";
             }
-            else{
-                x+=dirs[dir][0];
-                y+=dirs[dir][1];
+            else if (ch == 'R') {
+                if (directions.equals("North"))
+                    directions = "East";
+                else if (directions.equals("East"))
+                    directions = "South";
+                else if (directions.equals("South"))
+                    directions = "West";
+                else directions = "North";
             }
-            
         }
-        
-        return (x==0 && y==0) || dir>0;
+        if (x == 0 && y == 0)
+            return true;
+        if (directions.equals("North"))
+            return false;
+        return true;
     }
 }
+    
