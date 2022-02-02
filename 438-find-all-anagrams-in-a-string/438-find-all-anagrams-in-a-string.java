4b0e8378dd7574ae47174a9c261fd9f6b111493b
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2022-02-02 12:56:30 +0530

    Time: 24 ms (43.83%), Space: 48.6 MB (14.82%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,24 +1,39 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> ans = new ArrayList<>();
-        int n = p.length();
+        List<Integer> res = new ArrayList<>();
+        if(p.length()>s.length())
+            return res;
         
-        char[] arr = p.toCharArray();
-        Arrays.sort(arr);
-        String toCheck = String.valueOf(arr);
-        HashSet<String> set = new HashSet<>();
-        set.add(toCheck);
-        for(int i=0;i<=s.length()-n;i++){
-            
-            String sb =  s.substring(i,i+n);
-            char[] temp = sb.toCharArray();
-            Arrays.sort(temp);
-            
-            String curr = String.valueOf(temp);
-            if(set.contains(curr))
-                ans.add(i);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0;i<p.length();i++){
+            char cc = p.charAt(i);
+            map.put(cc, map.getOrDefault(cc,0)+1);
         }
         
-        return ans;
+        int begin =0,end =0,counter=map.size();
+        while(end<s.length()){
+            char cc = s.charAt(end);
+            if(map.containsKey(cc)){
+                map.put(cc, map.get(cc)-1);
+                if(map.get(cc)==0)
+                    counter--;
+            }
+            
+            end++;
+            while(counter==0){
+                char ci = s.charAt(begin);
+                if(map.containsKey(ci)){
+                    map.put(ci,map.get(ci)+1);
+                    if(map.get(ci)>0)
+                        counter++;
+                }
+            
+                if(end-begin==p.length())
+                    res.add(begin);
+                begin++;
+                    
+            }
+        }
+       return res;
     }
 }
