446946f93dfe6df5dcfb7b63920eb478740acc85
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2022-02-20 00:04:08 +0530

    Time: 165 ms (57.14%), Space: 87.9 MB (55.56%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
@@ -1,19 +1,24 @@
 class Solution {
     public int minimumDeviation(int[] A) {
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        int n = A.length, mi = Integer.MAX_VALUE, res = Integer.MAX_VALUE;
-        for (int a : A) {
-            if (a % 2 == 1) a *= 2;
-            pq.add(-a);
-            mi = Math.min(mi, a);
+        
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(b-a));
+        int min = Integer.MAX_VALUE, res = Integer.MAX_VALUE;
+        for(int i:A){
+            if(i%2==1)
+                i = i*2;
+            
+            pq.add(i);
+            min = Math.min(min, i);
         }
-        while (true) {
-            int a = -pq.poll();
-            res = Math.min(res, a - mi);
-            if (a % 2 == 1) break;
-            mi = Math.min(mi, a / 2);
-            pq.add(-a / 2);
+        
+        while(pq.peek()%2==0){
+            int curr = pq.poll();
+            res = Math.min(res, curr - min);
+            min = Math.min(min, curr/2);
+            pq.add(curr/2);
         }
-        return res;
+        
+        return Math.min(res, pq.peek()-min);
     }
 }
