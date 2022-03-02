70608cb77b2823712a74b6a4d38aa87c00ec6c44
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2022-03-02 23:10:52 +0530

    Time: 3 ms (23.87%), Space: 42.9 MB (6.86%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,16 +1,24 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-        int i=0,j=0;
-        while(i<s.length() && j<t.length()){
-            if(s.charAt(i)==t.charAt(j)){
-                i++;
-                j++;
-            }
-            else{
-                j++;
-            }
+     public boolean isSubsequence(String s, String t) {
+        List<Integer>[] idx = new List[256]; 
+         
+        for (int i = 0; i < t.length(); i++) {
+            if (idx[t.charAt(i)] == null)
+                idx[t.charAt(i)] = new ArrayList<>();
+            idx[t.charAt(i)].add(i);
         }
         
-        return i == s.length();
+        int prev = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (idx[s.charAt(i)] == null) 
+                return false; 
+            
+            int j = Collections.binarySearch(idx[s.charAt(i)], prev);
+            if (j < 0) j = -j - 1;
+            if (j == idx[s.charAt(i)].size()) 
+                return false;
+            prev = idx[s.charAt(i)].get(j) + 1;
+        }
+        return true;
     }
 }
