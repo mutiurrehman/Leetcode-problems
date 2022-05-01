928b4ab29ad47b670ad076ca4473c03ec9e56c59
Mutiur Rehman Khan <60111991+mutiurrehman@users.noreply.github.com>
2022-05-01 23:49:57 +0530

    Time: 1 ms (87.56%), Space: 42.3 MB (45.95%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,36 +1,24 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        
-        Stack<Character> st1 = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!='#')
-                st1.push(s.charAt(i));
-            else{
-                if(!st1.isEmpty())
-                    st1.pop();
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1, back;
+        while (true) {
+            back = 0;
+            while (i >= 0 && (back > 0 || S.charAt(i) == '#')) {
+                back += S.charAt(i) == '#' ? 1 : -1;
+                i--;
+            }
+            back = 0;
+            while (j >= 0 && (back > 0 || T.charAt(j) == '#')) {
+                back += T.charAt(j) == '#' ? 1 : -1;
+                j--;
+            }
+            if (i >= 0 && j >= 0 && S.charAt(i) == T.charAt(j)) {
+                i--;
+                j--;
+            } else {
+                break;
             }
         }
-        
-        String s1 = "";
-        while(st1.size()!=0){
-            s1+=st1.pop();
-        }
-        
-        String s2 = "";
-        st1 = new Stack<>();
-        for(int i=0;i<t.length();i++){
-            if(t.charAt(i)!='#')
-                st1.push(t.charAt(i));
-            else{
-                if(!st1.isEmpty())
-                    st1.pop();
-            }
-        }
-        
-        while(st1.size()!=0){
-            s2+=st1.pop();
-        }
-        
-        return s1.equals(s2);
+        return i == -1 && j == -1;
     }
 }
